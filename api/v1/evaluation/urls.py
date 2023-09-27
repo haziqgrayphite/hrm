df26172609a47cb85c2e277f9ffa69117dfda8c2
arslanfayyaz <arslanghouri500@gmail.com>
2023-09-27 18:55:26 +0500

    working on UpdateEvaluationScores

--- api/v1/evaluation/urls.py
+++ api/v1/evaluation/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import GeneralAPIView, EvaluationAPIView
+from .views import GeneralAPIView, EvaluationAPIView, UpdateEvaluationScores
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,6 +8,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('general_data/', GeneralAPIView.as_view(), name='general-api'),
     path('evaluation/', EvaluationAPIView.as_view(), name='evaluation-api'),
+    path('update-evaluation-scores/<int:evaluation_id>/', UpdateEvaluationScores.as_view(), name='update-evaluation-scores'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
--- api/v1/evaluation/views.py
+++ api/v1/evaluation/views.py
@@ -2,11 +2,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from .models import Evaluation, Parameter, ParameterRating, EvaluationScore
-from .serializers import ParameterSerializer, ParameterRatingSerializer, EvaluationSerializer, EvaluationScoreSerializer
+from .serializers import (ParameterSerializer, ParameterRatingSerializer, EvaluationSerializer
+                          , EvaluationScoreUpdateSerializer)
 
 
 class GeneralAPIView(APIView):
     def get(self, request):
+
         parameters = Parameter.objects.filter(is_active=True)
         parameter_ratings = ParameterRating.objects.filter(is_active=True)
 
@@ -24,6 +26,7 @@ class GeneralAPIView(APIView):
 class EvaluationAPIView(APIView):
 
     def get(self, request, evaluator_id=None):
+
         user = request.user
 
         evaluations = Evaluation.objects.filter(evaluator=user) | Evaluation.objects.filter(evaluatee=user)
@@ -31,3 +34,50 @@ class EvaluationAPIView(APIView):
         serializer = EvaluationSerializer(evaluations, many=True, context={'request': request})
 
         return Response(serializer.data)
+
+
+class UpdateEvaluationScores(APIView):
+    def put(self, request, evaluation_id):
+
+        try:
+
+            evaluation_scores_data = request.data.get('evaluation_scores', [])
+            evaluation = Evaluation.objects.get(id=evaluation_id)
+
+            for data in evaluation_scores_data:
+
+                parameter_id = data['parameter']
+                parameter_rating = data['parameter_rating']
+
+                try:
+
+                    evaluation_score = EvaluationScore.objects.get(evaluation=evaluation, parameter_id=parameter_id)
+                    serializer = EvaluationScoreUpdateSerializer(evaluation_score, data=data, partial=True)
+
+                    if serializer.is_valid():
+
+                        serializer.save()
+                        evaluation_score.is_evaluated = True
+                        evaluation_score.save()
+
+                    else:
+
+                        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+                except EvaluationScore.DoesNotExist:
+
+                    return Response(f"Parameter with ID {parameter_id} not found for this evaluation.",
+                                    status=status.HTTP_404_NOT_FOUND)
+
+            if all(evaluation_score.is_evaluated for evaluation_score in evaluation.evaluation_score.all()):
+
+                evaluation.is_evaluated = True
+                evaluation.save()
+
+            msg = "Parameter ratings and is_evaluated flag updated successfully."
+
+            return Response({'message': msg}, status=status.HTTP_201_CREATED)
+
+        except Evaluation.DoesNotExist:
+
+            return Response("Evaluation not found", status=status.HTTP_404_NOT_FOUND)
