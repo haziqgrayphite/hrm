357bc02a601540f61d20cb56efe843b698ed69a9
arslanfayyaz <arslanghouri500@gmail.com>
2023-09-28 17:19:55 +0500

    remove comments

--- api/v1/evaluation/admin.py
+++ api/v1/evaluation/admin.py
@@ -48,22 +48,16 @@ class BaseEvaluationAdmin(admin.ModelAdmin):
         parameter_names = [parameter.name for parameter in parameters]
         return parameter_names
 
-
     get_parameters.short_description = 'Parameters'
-
-    def parameters_display(self, obj):
-        return linebreaksbr(self.get_parameters(obj))
 
     def get_evaluators(self, obj):
         evaluators = obj.evaluators.all()
-        # return ', '.join([evaluator.username for evaluator in evaluators])
         return [evaluator.username for evaluator in evaluators]
 
     get_evaluators.short_description = 'Evaluators'
 
     def get_evaluatees(self, obj):
         evaluatees = obj.evaluatees.all()
-        # return ', '.join([evaluatee.username for evaluatee in evaluatees])
         return [evaluatee.username for evaluatee in evaluatees]
 
     get_evaluatees.short_description = 'Evaluatees'
