eb9b2c4048888fe10f06cabd6684798b6b7b24b7
arslanfayyaz <arslanghouri500@gmail.com>
2023-10-03 14:57:49 +0500

    make models for meal

---
+++ api/v1/meal/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django.utils import timezone
+from api.v1.accounts.models import CustomUser
+
+
+class Address(models.Model):
+    street_address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.street_address}, {self.city}, {self.state} {self.postal_code}"
+
+
+# Vendor Model
+class Vendor(models.Model):
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+
+    email = models.EmailField()
+
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='vendors')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    meals = models.ManyToManyField('Meal', related_name='menus')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+# Meal Model
+class Meal(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    date = models.DateField(default=timezone.now)
+    time = models.TimeField(default="15:00")
+
+    employee_wants_food = models.BooleanField(default=True)
+
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, null=True, blank=True, related_name="meals")
+    cost = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    attendees = models.ManyToManyField(CustomUser, related_name="meals_attending")
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name="reviews")
+    attendee = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="reviews_given")
+
+    rating = models.PositiveIntegerField(default=5)
+    comment = models.TextField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Rating for {self.meal} by {self.attendee}"
